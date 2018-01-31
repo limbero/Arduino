5b252880e28714075fac1e40f193ea9fe89a0f9b
Axel Samuelsson <harby@spotify.com>
2018-01-31 10:20:29 +0100

    comments

--- Remote/Remote.ino
+++ Remote/Remote.ino
@@ -1,13 +1,11 @@
 // one pin to ground and one to 3
-// volume down 4BB6C03F
-// volume up 4BB640BF
 #include <IRremote.h>
 IRsend irsend;
-//#define POWER         0xE0E040BF      //from IRrecvDemo, Ken Shirriff
-//#define SPEAKER_POWER 0x4FBE817       //recorded value
-//#define TV_POWER      0x2FD48B7       //
-#define VOLUME_UP     0x4BB640BF
-#define VOLUME_DOWN   0x4BB6C03F
+//#define POWER         0xE0E040BF      // from IRrecvDemo, Ken Shirriff
+//#define SPEAKER_POWER 0x4FBE817       // old speakers, recorded value, NEC
+//#define TV_POWER      0x2FD48B7       // toshiba TV, recorded value, NEC
+#define VOLUME_UP     0x4BB640BF      // onkyo amp, recorded value, NEC
+#define VOLUME_DOWN   0x4BB6C03F      // onkyo amp, recorded value, NEC
 int incoming = -1;
 
 void setup() {
